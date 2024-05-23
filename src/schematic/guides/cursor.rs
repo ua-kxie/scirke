1640a4d72d46c2fcf0c231e9ae410ae637622fb9
ua-kxie <kxie@ualberta.ca>
2024-05-23 20:04:47 +0800

    add cursor moved event

--- src/schematic/guides/cursor.rs
+++ src/schematic/guides/cursor.rs
@@ -13,13 +13,22 @@ use crate::{
 
 use super::{SchematicCamera, ZoomInvariant};
 
+/// event indicating a new cursor position. None indicates that the cursor moved off-window
+#[derive(Event, Deref, PartialEq)]
+pub struct NewSnappedCursor(pub Option<Vec2>);
+
 pub struct CursorPlugin;
 
 impl Plugin for CursorPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
-        app.add_systems(Update, update);
+        app.add_systems(Update, update.run_if(cursor_moved));
+        app.add_event::<NewSnappedCursor>();
     }
+}
+
+fn cursor_moved(mut ecm: EventReader<CursorMoved>) -> bool {
+    ecm.read().last().is_some()
 }
 
 #[derive(Component)]
@@ -37,11 +46,18 @@ impl Default for SchematicCursor {
     }
 }
 
+impl SchematicCursor {
+    fn as_new_event(&self) -> NewSnappedCursor {
+        NewSnappedCursor(self.coords.as_ref().map(|x| x.snapped_world_coords))
+    }
+}
+
+#[derive(Clone)]
 pub struct Coords {
-    pub screen_coords: Vec2,
+    screen_coords: Vec2,
     pub snapped_world_coords: Vec2,
-    pub world_coords: Vec2,
-    pub ndc_coords: Vec3,
+    world_coords: Vec2,
+    ndc_coords: Vec3,
 }
 
 #[derive(Bundle)]
@@ -59,10 +75,13 @@ fn update(
     q_window: Query<&Window, With<PrimaryWindow>>,
     q_camera: Query<(&Camera, &GlobalTransform), With<SchematicCamera>>,
     mut q_cursor: Query<(&mut SchematicCursor, &mut Visibility, &mut Transform)>,
+    mut e_new_snapped: EventWriter<NewSnappedCursor>,
 ) {
     let (mut c, mut visibility, mut c_t) = q_cursor.single_mut();
     let cam = q_camera.get_single();
     let window = q_window.get_single();
+    let new_coords;
+    let new_event;
     if cam.is_ok() && window.as_ref().is_ok_and(|w| w.cursor_position().is_some()) {
         let (cam, cgt) = cam.unwrap();
         let window = window.unwrap();
@@ -72,23 +91,34 @@ fn update(
                 .world_to_ndc(cgt, world_coords.extend(c_t.translation.z))
                 .unwrap();
             let snapped_world_coords = (world_coords / c.snap_step).round() * c.snap_step;
+
             *visibility = Visibility::Visible;
-            c.coords = Some(Coords {
+            new_coords = Some(Coords {
                 screen_coords,
                 world_coords,
                 ndc_coords,
                 snapped_world_coords,
             });
+            new_event = NewSnappedCursor(Some(snapped_world_coords));
 
+            // snap the cursor position
             *c_t = c_t.with_translation(snapped_world_coords.extend(c_t.translation.z));
         } else {
             *visibility = Visibility::Hidden;
-            c.coords = None;
+            new_coords = None;
+            new_event = NewSnappedCursor(None);
         }
     } else {
         *visibility = Visibility::Hidden;
-        c.coords = None;
+        new_coords = None;
+        new_event = NewSnappedCursor(None);
     }
+    // see whats changed and maybe write an event
+    if c.as_new_event() != new_event {
+        e_new_snapped.send(new_event);
+    }
+    // write the new coords
+    c.coords = new_coords;
 }
 
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<SchematicMaterial>>) {
